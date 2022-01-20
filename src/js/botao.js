5e3e4b33d42a22227bd3897ee127b99225b7cc76
Washington Bezerra <luiswashington00@gmail.com>
2022-01-20 11:29:20 -0300

    remoção de linha inutil e added alert

--- src/js/botao.js
+++ src/js/botao.js
@@ -3,13 +3,14 @@ function desabilita(){
 
     for(var i = 0; i < campos.length; i++){
 
+        //edita somente dos campos
         if(campos[i].tagName != 'LABEL'){
-        campos[i].disabled = true;
-        campos[i].style.cursor = 'not-allowed';
-        campos[i].style.backgroundColor = 'rgb(197, 197, 197)';  
+            
+            campos[i].disabled = true;
+            campos[i].style.cursor = 'not-allowed';
+            campos[i].style.backgroundColor = 'rgb(197, 197, 197)';  
         }
     }
 
-    document.getElementByName("button-submit").disabled = true;
-
+    alert("Sua mensagem foi enviada com sucesso");
 }
