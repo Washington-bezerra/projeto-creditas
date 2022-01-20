a8f0b84821c5b34d12445d9e9bbd318edc32500a
Washington Bezerra <luiswashington00@gmail.com>
2022-01-20 14:45:24 -0300

    refatoramento no logo + nova imagem

---
+++ logo.js
@@ -0,0 +1,3 @@
+document.write(`
+<img src="src/images/Design sem nome.png" style="border-radius: 8px; margin: 0px 0px 16px 0px">
+`)
---
+++ menu.js
@@ -0,0 +1,11 @@
+document.write(`
+<div class="box column">
+    <ul>
+        <li><a href="/index.html">Home</a></li>
+        <li><a href="/sushi.html">Sushi</a></li>
+        <li><a href="/shushi-premium.html">Sushi premium</a></li>
+        <li><a href="/contato.html">Contato</a></li>
+        <li><a href="/quem-somos.html">Quem somos</a></li>
+    </ul>
+</div>
+`)
