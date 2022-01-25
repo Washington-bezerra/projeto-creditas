9ab549310b83e93f36f9ca2029726a899c57e7b9
Washington Bezerra <luiswashington00@gmail.com>
2022-01-25 15:28:01 -0300

    fixo-meio pronto

--- 2- Fixo-meio/src/js/logo.js
+++ 2- Fixo-meio/src/js/logo.js
@@ -1,7 +1,7 @@
 document.write(`
 <header>
 <figure>
-<img src="src/images/capa.png" style="border-radius: 8px; margin: 0px 0px 16px 0px; width: 100%">
+<img src="src/images/capa.png" style="border-radius: 8px; margin: 0px 0px 16px 0px; width: 940px">
 </figure>
 </header>
 `)
