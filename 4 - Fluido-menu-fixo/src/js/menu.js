a0742d7012e78d0329a16ad2ad33fd2467bbadba
washington-bezerra <luiswashington00@gmail.com>
2022-02-01 10:31:48 -0300

    Versão  pronta

--- 4 - Fluido-menu-fixo/src/js/menu.js
+++ 4 - Fluido-menu-fixo/src/js/menu.js
@@ -1,9 +1,9 @@
 document.write(`
-<nav class="menu column">
+<nav class="menu">
     <ul>
         <li><a href="./index.html">Home</a></li>
         <li><a href="./sushi.html">Sushi</a></li>
-        <li><a href="./shushi-premium.html">Sushi premium</a></li>
+        <li><a href="./sushi-premium.html">Sushi premium</a></li>
         <li><a href="./contato.html">Contato</a></li>
         <li><a href="./quem-somos.html">Quem somos</a></li>
     </ul>
